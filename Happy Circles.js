b79d00a28916b4b3a542fd08c1719b4523aa4bf6
hessaal <algamilh8@gmail.com>
2020-11-14 11:15:02 +0300

    Update Happy Circles.js

--- Happy Circles.js
+++ Happy Circles.js
@@ -1,20 +1,23 @@
 var canvas = document.querySelector("canvas");
 var ctx = canvas.getContext("2d");
-var balls =[];
-var ball = 0 ;
-var mySound;
+var balls =[];//array of ball object
+var mySound; // object from sound class
+
+//check if is it touch or mouse event 
 var istouch = false;
 var ismouse = false;
-canvas.width =window.innerWidth;
+
+// let the canvas width and height get the window width and hight
+canvas.width =window.innerWidth; 
 canvas.height = window.innerHeight;
 
-
+//funcition that return random number from range that spicfy by the parameters max and min
 function random(max , min) {
   var num = Math.floor(Math.random() * (max - min) + min) ;
   return num;
 }
 
-
+//class sound that create audio element and take the src as parameter for his consrctor 
 function sound(src) {
     this.sound = document.createElement("audio");
     this.sound.src = src;
@@ -24,22 +27,24 @@ function sound(src) {
     document.body.appendChild(this.sound);
     this.play = function(){
         this.sound.play( );
-    }
-    this.stop = function(){
-        this.sound.pause();
-    }    
-}
+    }}
+
+//class mouse that track the pointer x and y cooradints  
 function mouse(x,y){
   this.dx = x;
-  this.dy = y;}
+  this.dy = y;
+}
+
+//object from mouse class
 var mousePos = new mouse(0,0);
 
+//canvas event handlers for the touch events
 canvas.addEventListener("touchstart", function(evt){
   istouch=true;
   ismouse = false;
  mousePos.dx = evt.touches[0].clientX;
  mousePos.dy = evt.touches[0].clientY;
- console.log("in touch move  " + mousePos.dx );
+ evt.preventDefault();
 });
 
 canvas.addEventListener("touchmove", function(evt){
@@ -47,16 +52,20 @@ canvas.addEventListener("touchmove", function(evt){
  ismouse = false;
  mousePos.dx = evt.touches[0].clientX;
  mousePos.dy = evt.touches[0].clientY;
- console.log("in touch move  " + mousePos.dx );
-});
+ evt.preventDefault();
 
+});
 
 canvas.addEventListener("touchend", function(evt){
  mousePos.dx = 0;
  mousePos.dy = 0;
- console.log("in touch end  " + mousePos.dx );
+ evt.preventDefault();
 });
 
+canvas.addEventListener("touchcancel", function(evt){
+ evt.preventDefault();
+});
+//canvas event handlers for the mouse events
 canvas.addEventListener('mousemove', function(evt) {
 if (istouch){
   evt.preventDefault();
@@ -70,7 +79,7 @@ mousePos.dy = evt.clientY;
 
 });
 
-canvas.addEventListener('mouseout', function(evt) {
+canvas.addEventListener('mouseout', function() {
   if (istouch){
   evt.preventDefault();
   istouch = false;
@@ -80,6 +89,7 @@ mousePos.dx = 0;
 mousePos.dy = 0;
 });
 
+//class Ball that create balls
 function Ball(){
   this.dx = random(6,0);
   this.dy = random(8,2);
@@ -89,6 +99,8 @@ function Ball(){
   this.bx = random(maxWidth,this.size+1); 
   this.by = random(maxHight,this.size+1);
   this.color = "rgba(" + random(255,0) + ", " + random(255,0) + ", " + random(255,0) + ", " + Math.random()*1 + ")";
+
+//function that move ball object
   this.moveball=function(){
  ctx.fillStyle = this.color ;
  ctx.beginPath();
@@ -107,6 +119,7 @@ function Ball(){
     this.by+=this.dy;
 };
 
+//function that draw bigger ball object
 this.drawbigger = function() {
 ctx.fillStyle = this.color ;
 ctx.beginPath();
@@ -125,7 +138,8 @@ ctx.fill();
     this.by+=this.dy;
 };
 
-this.bigger = function() {
+//function check if pointer overlap with ball object
+this.eventoverlap = function() {
 if (mousePos.dx <= (this.bx + this.size) &&  mousePos.dx >= (this.bx - this.size)){
 if (mousePos.dy <= (this.by + this.size) &&  mousePos.dy >= (this.by - this.size)){
 	return true;
@@ -135,18 +149,21 @@ if (mousePos.dy <= (this.by + this.size) &&  mousePos.dy >= (this.by - this.size
 };
 }
 
+//create ball objects in the balls array
 for (var i = 0 ; i < 70 ; i++){
 	balls[i] = new Ball();
 }
-mySound = new sound("https://raw.githubusercontent.com/hessaal/HappyCircles.github.io/main/boing2.wav");
+
+
+mySound = new sound("boing2.wav");
 
 
 function start(){
-canvas.width =window.innerWidth;
+canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 ctx.clearRect(0, 0, canvas.width, canvas.height);
 for (var i = 0 ; i < 70 ; i++){
-	if (balls[i].bigger()) {
+	if (balls[i].eventoverlap()) {
   mySound.play();
 	balls[i].drawbigger();
 }else
